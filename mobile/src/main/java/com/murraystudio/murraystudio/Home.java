bcd58c761595eb55bf7bf2a8399619d23542d1c6
Murray Studio <sushishamus@gmail.com>
2015-06-20 18:21:30 -0700

    cardview added

--- mobile/src/main/java/com/murraystudio/murraystudio/Home.java
+++ mobile/src/main/java/com/murraystudio/murraystudio/Home.java
@@ -2,6 +2,7 @@ package com.murraystudio.murraystudio;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,15 +11,19 @@ import android.text.style.UnderlineSpan;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 
-public class Home extends Fragment {
+public class Home extends Fragment implements BaseSliderView.OnSliderClickListener {
 
 	android.support.v7.app.ActionBar actionBar;
 	Button playStore;
@@ -26,6 +31,7 @@ public class Home extends Fragment {
 	TextView changelog;
 	private final static String APP_NAME = "com.murraystudio.storystudiopro";
 	SliderLayout sliderShow;
+	ImageButton logo1;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,8 +50,7 @@ public class Home extends Fragment {
 		getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
 		int screenHeight = displaymetrics.heightPixels;
 		int screenWidth = displaymetrics.widthPixels;
-		double h = 0.357;
-		double w = 1.0;
+		double w = 0.975;
 		int imgWidth =  (int) (screenWidth* w);
 		int imgHeight =  (int) (imgWidth/2.8);
 
@@ -55,28 +60,66 @@ public class Home extends Fragment {
 		sliderShow.getLayoutParams().height = imgHeight;
 		sliderShow.getLayoutParams().width = imgWidth;
 
+
+		//add slides into slideshow
+
 		TextSliderView textSliderView1 = new TextSliderView(getActivity());
 		textSliderView1
 				.description("Murray Studio")
-				.image("http://i.imgur.com/m9LupJ3.jpg");
+				.image("http://i.imgur.com/m9LupJ3.jpg")
+				.setOnSliderClickListener(this);
 
 		sliderShow.addSlider(textSliderView1);
 
 		TextSliderView textSliderView2 = new TextSliderView(getActivity());
 		textSliderView2
 				.description("Story Studio")
-				.image("http://i.imgur.com/jcrsUwP.jpg");
+				.image("http://i.imgur.com/jcrsUwP.jpg")
+				.setOnSliderClickListener(this);
 
 		sliderShow.addSlider(textSliderView2);
+
+		TextSliderView textSliderView3 = new TextSliderView(getActivity());
+		textSliderView3
+				.description("Risk")
+				.image("http://i.imgur.com/w4y8ENR.jpg")
+				.setOnSliderClickListener(this);
+
+		sliderShow.addSlider(textSliderView3);
+
+
+
+		logo1 = (ImageButton) view.findViewById(R.id.logo1);
+		logo1.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+
+				switch (event.getAction()) {
+					case MotionEvent.ACTION_DOWN:
+						logo1.setColorFilter(Color.argb(35, 255, 255, 255)); // White Tint
+						return true; // if you want to handle the touch event
+					case MotionEvent.ACTION_UP:
+						logo1.clearColorFilter(); // White Tint
+						return true; // if you want to handle the touch event
+				}
+				return false;
+			}
+		});
+
 		
 		return view;
-
     }
 
 	@Override
 	public void onStop() {
 		sliderShow.stopAutoCycle();
 		super.onStop();
+	}
+
+	//when slider is clicked
+	@Override
+	public void onSliderClick(BaseSliderView baseSliderView) {
+		Toast.makeText(getActivity(), baseSliderView.getDescription().toString() + "",Toast.LENGTH_SHORT).show();
 	}
 }
 
