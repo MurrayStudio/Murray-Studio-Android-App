a62b1c0eacc5970119f8f5f21a82121b42e21b98
Murray Studio <sushishamus@gmail.com>
2015-07-05 21:31:41 -0700

    recyclerview implemented

--- mobile/src/main/java/com/murraystudio/murraystudio/Home.java
+++ mobile/src/main/java/com/murraystudio/murraystudio/Home.java
@@ -1,24 +1,14 @@
 package com.murraystudio.murraystudio;
 
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.slider.library.SliderLayout;
@@ -27,31 +17,57 @@ import com.daimajia.slider.library.SliderTypes.TextSliderView;
 
 public class Home extends Fragment implements BaseSliderView.OnSliderClickListener {
 
+	private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+	private static final int SPAN_COUNT = 2;
+	private static final int DATASET_COUNT = 60;
+
+	private enum LayoutManagerType {
+		GRID_LAYOUT_MANAGER,
+		LINEAR_LAYOUT_MANAGER
+	}
+
 	android.support.v7.app.ActionBar actionBar;
-	Button playStore;
-	Button playStore2;
-	TextView changelog;
 	private final static String APP_NAME = "com.murraystudio.storystudiopro";
 	SliderLayout sliderShow;
-	RecyclerView rv;
-	LinearLayoutManager llm;
+	protected RecyclerView mRecyclerView;
+	protected CustomAdapter mAdapter;
+	protected RecyclerView.LayoutManager mLayoutManager;
+	protected LayoutManagerType mCurrentLayoutManagerType;
+	protected String[] mDataset;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Initialize dataset, this data would usually come from a local content provider or
+		// remote server.
+		initDataset();
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-
 		View view = inflater.inflate(R.layout.home, container, false);
+
+
 
 		actionBar = (android.support.v7.app.ActionBar) ((MainActivity) getActivity())
 				.getSupportActionBar();
 		actionBar.setTitle("About");
 
-		rv = (RecyclerView) view.findViewById(R.id.cardList);
-		rv.setHasFixedSize(true);
+		mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+		mLayoutManager = new LinearLayoutManager(getActivity());
+		mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+		if (savedInstanceState != null) {
+			// Restore saved layout manager type.
+			mCurrentLayoutManagerType = (LayoutManagerType) savedInstanceState
+					.getSerializable(KEY_LAYOUT_MANAGER);
+		}
+		setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
 
-		llm = new LinearLayoutManager(getActivity());
-		rv.setLayoutManager(llm);
+		mAdapter = new CustomAdapter(mDataset);
+		mRecyclerView.setAdapter(mAdapter);
 
 		//calculate slideshow height and width based off screen dimensions and
 		//image size of 1400x500
@@ -100,6 +116,56 @@ public class Home extends Fragment implements BaseSliderView.OnSliderClickListen
 		
 		return view;
     }
+
+	/**
+	 * Set RecyclerView's LayoutManager to the one given.
+	 *
+	 * @param layoutManagerType Type of layout manager to switch to.
+	 */
+	public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+		int scrollPosition = 0;
+
+		// If a layout manager has already been set, get current scroll position.
+		if (mRecyclerView.getLayoutManager() != null) {
+			scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
+					.findFirstCompletelyVisibleItemPosition();
+		}
+
+		switch (layoutManagerType) {
+			case GRID_LAYOUT_MANAGER:
+				mLayoutManager = new GridLayoutManager(getActivity(), SPAN_COUNT);
+				mCurrentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
+				break;
+			case LINEAR_LAYOUT_MANAGER:
+				mLayoutManager = new LinearLayoutManager(getActivity());
+				mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+				break;
+			default:
+				mLayoutManager = new LinearLayoutManager(getActivity());
+				mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+		}
+
+		mRecyclerView.setLayoutManager(mLayoutManager);
+		mRecyclerView.scrollToPosition(scrollPosition);
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		// Save currently selected layout manager.
+		savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
+		super.onSaveInstanceState(savedInstanceState);
+	}
+
+	/**
+	 * Generates Strings for RecyclerView's adapter. This data would usually come
+	 * from a local content provider or remote server.
+	 */
+	private void initDataset() {
+		mDataset = new String[DATASET_COUNT];
+		for (int i = 0; i < DATASET_COUNT; i++) {
+			mDataset[i] = "This is element #" + i;
+		}
+	}
 
 	@Override
 	public void onStop() {
