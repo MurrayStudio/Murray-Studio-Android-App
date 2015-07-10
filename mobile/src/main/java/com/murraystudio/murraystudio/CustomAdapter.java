d8d882542f38ef38acd6bedaf155bcf9228f4f96
Murray Studio <sushishamus@gmail.com>
2015-07-09 21:35:31 -0700

    recycleview changes

--- mobile/src/main/java/com/murraystudio/murraystudio/CustomAdapter.java
+++ mobile/src/main/java/com/murraystudio/murraystudio/CustomAdapter.java
@@ -16,11 +16,14 @@
 
 package com.murraystudio.murraystudio;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 /**
@@ -30,7 +33,10 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     private static final String TAG = "CustomAdapter";
 
     private String[] mDataSet;
+    // Allows to remember the last item shown on screen
+    private int lastPosition = -1;
 
+    private Context context;
     /**
      * Provide a reference to the type of views that you are using (custom ViewHolder)
      */
@@ -59,8 +65,9 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
      *
      * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
      */
-    public CustomAdapter(String[] dataSet) {
+    public CustomAdapter(String[] dataSet, Context context) {
         mDataSet = dataSet;
+        this.context = context;
     }
 
     // Create new views (invoked by the layout manager)
@@ -68,7 +75,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         // Create a new view.
         View v = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.text_row_item, viewGroup, false);
+                .inflate(R.layout.card_item, viewGroup, false);
 
         return new ViewHolder(v);
     }
@@ -81,6 +88,27 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         // Get element from your dataset at this position and replace the contents of the view
         // with that element
         viewHolder.getTextView().setText(mDataSet[position]);
+
+        // Here you apply the animation when the view is bound
+        setAnimation(viewHolder.itemView, position);
+    }
+
+    private void setAnimation(View viewToAnimate, int position)
+    {
+
+        // If the bound view wasn't previously displayed on screen, it's animated
+        if (position > lastPosition)
+        {
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.push_up);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(ViewHolder holder) {
+        super.onViewDetachedFromWindow(holder);
+        holder.itemView.clearAnimation();
     }
 
     // Return the size of your dataset (invoked by the layout manager)
--- mobile/src/main/java/com/murraystudio/murraystudio/Home.java
+++ mobile/src/main/java/com/murraystudio/murraystudio/Home.java
@@ -1,8 +1,9 @@
 package com.murraystudio.murraystudio;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
@@ -17,166 +18,145 @@ import com.daimajia.slider.library.SliderTypes.TextSliderView;
 
 public class Home extends Fragment implements BaseSliderView.OnSliderClickListener {
 
-	private static final String KEY_LAYOUT_MANAGER = "layoutManager";
-	private static final int SPAN_COUNT = 2;
-	private static final int DATASET_COUNT = 60;
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    private static final int DATASET_COUNT = 60;
 
-	private enum LayoutManagerType {
-		GRID_LAYOUT_MANAGER,
-		LINEAR_LAYOUT_MANAGER
-	}
+    android.support.v7.app.ActionBar actionBar;
+    SliderLayout sliderShow;
+    protected RecyclerView mRecyclerView;
+    protected CustomAdapter mAdapter;
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected String[] mDataset;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
-	android.support.v7.app.ActionBar actionBar;
-	private final static String APP_NAME = "com.murraystudio.storystudiopro";
-	SliderLayout sliderShow;
-	protected RecyclerView mRecyclerView;
-	protected CustomAdapter mAdapter;
-	protected RecyclerView.LayoutManager mLayoutManager;
-	protected LayoutManagerType mCurrentLayoutManagerType;
-	protected String[] mDataset;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		// Initialize dataset, this data would usually come from a local content provider or
-		// remote server.
-		initDataset();
-	}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		View view = inflater.inflate(R.layout.home, container, false);
-
-
-
-		actionBar = (android.support.v7.app.ActionBar) ((MainActivity) getActivity())
-				.getSupportActionBar();
-		actionBar.setTitle("About");
-
-		mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
-		mLayoutManager = new LinearLayoutManager(getActivity());
-		mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
-		if (savedInstanceState != null) {
-			// Restore saved layout manager type.
-			mCurrentLayoutManagerType = (LayoutManagerType) savedInstanceState
-					.getSerializable(KEY_LAYOUT_MANAGER);
-		}
-		setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
-
-		mAdapter = new CustomAdapter(mDataset);
-		mRecyclerView.setAdapter(mAdapter);
-
-		//calculate slideshow height and width based off screen dimensions and
-		//image size of 1400x500
-		DisplayMetrics displaymetrics = new DisplayMetrics();
-		getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-		int screenHeight = displaymetrics.heightPixels;
-		int screenWidth = displaymetrics.widthPixels;
-		double w = 1.00;
-		int imgWidth =  (int) (screenWidth* w);
-		int imgHeight =  (int) (imgWidth/2.8);
-
-		sliderShow = (SliderLayout) view.findViewById(R.id.slider);
-
-		//set new height and width for slideshow
-		sliderShow.getLayoutParams().height = imgHeight;
-		sliderShow.getLayoutParams().width = imgWidth;
-
-
-		//add slides into slideshow
-
-		TextSliderView textSliderView1 = new TextSliderView(getActivity());
-		textSliderView1
-				.description("Murray Studio")
-				.image("http://i.imgur.com/m9LupJ3.jpg")
-				.setOnSliderClickListener(this);
-
-		sliderShow.addSlider(textSliderView1);
-
-		TextSliderView textSliderView2 = new TextSliderView(getActivity());
-		textSliderView2
-				.description("Story Studio")
-				.image("http://i.imgur.com/jcrsUwP.jpg")
-				.setOnSliderClickListener(this);
-
-		sliderShow.addSlider(textSliderView2);
-
-		TextSliderView textSliderView3 = new TextSliderView(getActivity());
-		textSliderView3
-				.description("Risk")
-				.image("http://i.imgur.com/w4y8ENR.jpg")
-				.setOnSliderClickListener(this);
-
-		sliderShow.addSlider(textSliderView3);
-
-
-		
-		return view;
+        // Initialize dataset, this data would usually come from a local content provider or
+        // remote server.
+        initDataset();
     }
 
-	/**
-	 * Set RecyclerView's LayoutManager to the one given.
-	 *
-	 * @param layoutManagerType Type of layout manager to switch to.
-	 */
-	public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
-		int scrollPosition = 0;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.home, container, false);
 
-		// If a layout manager has already been set, get current scroll position.
-		if (mRecyclerView.getLayoutManager() != null) {
-			scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
-					.findFirstCompletelyVisibleItemPosition();
-		}
 
-		switch (layoutManagerType) {
-			case GRID_LAYOUT_MANAGER:
-				mLayoutManager = new GridLayoutManager(getActivity(), SPAN_COUNT);
-				mCurrentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
-				break;
-			case LINEAR_LAYOUT_MANAGER:
-				mLayoutManager = new LinearLayoutManager(getActivity());
-				mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
-				break;
-			default:
-				mLayoutManager = new LinearLayoutManager(getActivity());
-				mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
-		}
+        actionBar = (android.support.v7.app.ActionBar) ((MainActivity) getActivity())
+                .getSupportActionBar();
+        actionBar.setTitle("About");
 
-		mRecyclerView.setLayoutManager(mLayoutManager);
-		mRecyclerView.scrollToPosition(scrollPosition);
-	}
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(getActivity());
 
-	@Override
-	public void onSaveInstanceState(Bundle savedInstanceState) {
-		// Save currently selected layout manager.
-		savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
-		super.onSaveInstanceState(savedInstanceState);
-	}
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-	/**
-	 * Generates Strings for RecyclerView's adapter. This data would usually come
-	 * from a local content provider or remote server.
-	 */
-	private void initDataset() {
-		mDataset = new String[DATASET_COUNT];
-		for (int i = 0; i < DATASET_COUNT; i++) {
-			mDataset[i] = "This is element #" + i;
-		}
-	}
+        mAdapter = new CustomAdapter(mDataset, getActivity());
+        mRecyclerView.setAdapter(mAdapter);
 
-	@Override
-	public void onStop() {
-		sliderShow.stopAutoCycle();
-		super.onStop();
-	}
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
 
-	//when slider is clicked
-	@Override
-	public void onSliderClick(BaseSliderView baseSliderView) {
-		Toast.makeText(getActivity(), baseSliderView.getDescription().toString() + "",Toast.LENGTH_SHORT).show();
-	}
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Refresh items
+                refreshItems();
+            }
+        });
+
+        //calculate slideshow height and width based off screen dimensions and
+        //image size of 1400x500
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int screenHeight = displaymetrics.heightPixels;
+        int screenWidth = displaymetrics.widthPixels;
+        double w = 1.00;
+        int imgWidth = (int) (screenWidth * w);
+        int imgHeight = (int) (imgWidth / 2.8);
+
+        sliderShow = (SliderLayout) view.findViewById(R.id.slider);
+
+        //set new height and width for slideshow
+        sliderShow.getLayoutParams().height = imgHeight;
+        sliderShow.getLayoutParams().width = imgWidth;
+
+
+        //add slides into slideshow
+
+        TextSliderView textSliderView1 = new TextSliderView(getActivity());
+        textSliderView1
+                .description("Murray Studio")
+                .image("http://i.imgur.com/m9LupJ3.jpg")
+                .setOnSliderClickListener(this);
+
+        sliderShow.addSlider(textSliderView1);
+
+        TextSliderView textSliderView2 = new TextSliderView(getActivity());
+        textSliderView2
+                .description("Story Studio")
+                .image("http://i.imgur.com/jcrsUwP.jpg")
+                .setOnSliderClickListener(this);
+
+        sliderShow.addSlider(textSliderView2);
+
+        TextSliderView textSliderView3 = new TextSliderView(getActivity());
+        textSliderView3
+                .description("Risk")
+                .image("http://i.imgur.com/w4y8ENR.jpg")
+                .setOnSliderClickListener(this);
+
+        sliderShow.addSlider(textSliderView3);
+
+
+        return view;
+    }
+
+    /**
+     * Generates Strings for RecyclerView's adapter. This data would usually come
+     * from a local content provider or remote server.
+     */
+    private void initDataset() {
+        mDataset = new String[DATASET_COUNT];
+        for (int i = 0; i < DATASET_COUNT; i++) {
+            mDataset[i] = "This is element #" + i;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        sliderShow.stopAutoCycle();
+        super.onStop();
+    }
+
+    //when slider is clicked
+    @Override
+    public void onSliderClick(BaseSliderView baseSliderView) {
+        Toast.makeText(getActivity(), baseSliderView.getDescription().toString() + "", Toast.LENGTH_SHORT).show();
+    }
+
+    //refresh swipe methods
+    void refreshItems() {
+        mDataset = new String[DATASET_COUNT];
+        for (int i = 0; i < DATASET_COUNT; i++) {
+            mDataset[i] = "This is butt #" + i;
+        }
+
+        mAdapter = new CustomAdapter(mDataset, getActivity());
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Load complete
+        onItemsLoadComplete();
+    }
+
+    void onItemsLoadComplete() {
+        // Update the adapter and notify data set changed
+        // ...
+
+        // Stop refresh animation
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
 }
 
